2963e9dccf992fe08adf706caa71888bdc1ed865
难忘的旋律 <47502008+nwdxlgzs@users.noreply.github.com>
2023-07-07 14:39:54 +0800

    Update conf.js

--- conf.js
+++ conf.js
@@ -12,23 +12,23 @@ jsproxy_config({
 
   // 节点配置
   node_map: {
-    'demo-hk': {
-      label: '演示服务-香港节点',
-      lines: {
-        // 主机:权重
-        'node-aliyun-hk-0.etherdream.com:8443': 1,
-        'node-aliyun-hk-1.etherdream.com:8443': 1,
-        'node-aliyun-hk-2.etherdream.com:8443': 1,
-        'node-aliyun-hk-3.etherdream.com:8443': 1,
-        'node-aliyun-hk-4.etherdream.com:8443': 1,
-      }
-    },
-    'demo-sg': {
-      label: '演示服务-新加坡节点',
-      lines: {
-        'node-aliyun-sg.etherdream.com:8443': 1,
-      },
-    },
+    // 'demo-hk': {
+    //   label: '演示服务-香港节点',
+    //   lines: {
+    //     // 主机:权重
+    //     'node-aliyun-hk-0.etherdream.com:8443': 1,
+    //     'node-aliyun-hk-1.etherdream.com:8443': 1,
+    //     'node-aliyun-hk-2.etherdream.com:8443': 1,
+    //     'node-aliyun-hk-3.etherdream.com:8443': 1,
+    //     'node-aliyun-hk-4.etherdream.com:8443': 1,
+    //   }
+    // },
+    // 'demo-sg': {
+    //   label: '演示服务-新加坡节点',
+    //   lines: {
+    //     'node-aliyun-sg.etherdream.com:8443': 1,
+    //   },
+    // },
     'mysite': {
       label: '当前站点',
       lines: {
